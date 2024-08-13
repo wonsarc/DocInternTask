0b841f5a5b3890bdcbd900e7b1955abbfd6050f9
a.krasnov <wonsarc@icloud.com>
2024-08-13 10:56:24 +0300

    Initial Commit

---
+++ DocInternTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocInternTask
+//
+//  Created by Artem Krasnov on 13.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DocInternTask/DocInternTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocInternTaskApp.swift
+//  DocInternTask
+//
+//  Created by Artem Krasnov on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DocInternTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
